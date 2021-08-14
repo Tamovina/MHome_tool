2530265cd17e7098180a080f075c0c68a74523ae
Tamovina <88781745+Tamovina@users.noreply.github.com>
2021-08-14 15:55:42 +0700

    Update install-button.ts

--- src/install-button.ts
+++ src/install-button.ts
@@ -104,7 +104,7 @@ export class InstallButton extends HTMLElement {
 
     slot.name = "activate";
     const button = document.createElement("button");
-    button.innerText = "INSTALL";
+    button.innerText = "Cài đặt";
     slot.append(button);
     if (
       "adoptedStyleSheets" in Document.prototype &&
